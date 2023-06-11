afb75edca05be72a18d0c8f93abc46f559ff931d
oriyalperin <oriya2011@gmail.com>
2023-06-11 15:34:52 +0300

    update rmm

--- networky/algorithms/bipartite/rank_maximal_matching.py
+++ networky/algorithms/bipartite/rank_maximal_matching.py
@@ -1,11 +1,9 @@
-import networky as nx
-
-bipartite_sets = nx.algorithms.bipartite.sets
+import networkx as nx
+from networkx.algorithms.bipartite import sets as bipartite_sets
 
 
 def rank_maximal_matching(G, rank="rank", top_nodes=None):
     """Returns the rank maximal matching of the ranked bipartite graph `G`.
-
     A ranked graph is a graph in which every edge has a rank [1,r]
     (the algorithm ignores non-positive ranks)
     such that 1 is the highest rank, and then 2 is the next highest rank, and so on.
@@ -13,65 +11,54 @@ def rank_maximal_matching(G, rank="rank", top_nodes=None):
     A rank-maximal matching is one with the maximum
     possible number of edges with the first rank, and subject to that condition,
     the maximum possible number of edges with the second rank, and so on.
-
     Parameters
     ----------
     G : NetworkX graph
       Undirected weighted (the weight of every edge represents the rank) bipartite graph
-
     Returns
     -------
     M : dictionary
        The matching is returned as a dictionary, `matching`, such that
          ``matching[v] == w`` if node `v` is matched to node `w`. Unmatched
         nodes do not occur as a key in `matching`.
-
     Examples
     --------
     In the bipartite graph, G = (V,E). with the sets V1 as 0 and V2 as 1,
     and the weight of the edges as the ranks.
         >>> G = nx.Graph()
-        >>> G.add_nodes_from(['a1', 'a2'])
-        >>> G.add_nodes_from(['p1', 'p2'])
-        >>> G.add_weighted_edges_from([('a1', 'p1', 2),
-        ... ('a1', 'p2', 1), ('a2', 'p2', 2)])
+        >>> G.add_nodes_from(['a1', 'a2'], bipartite=0)
+        >>> G.add_nodes_from(['p1', 'p2'], bipartite=1)
+        >>> G.add_weighted_edges_from([('a1', 'p1', 2), ('a1', 'p2', 1), ('a2', 'p2', 2)])
         >>> M = rank_maximal_matching(G, rank="weight",top_nodes=['a1', 'a2'])
         >>> print(M)
         {'a1': 'p2', 'p2': 'a1'}
         >>> M['a1']
         'p2'
-
-    explanation:                 2
-                    G =   a1-----------p1
-                            \
-                             \
-                              \
-                               \
-                                \\ 1
-                                 \
-                                  \
-                                   \
-                              2     \
-                       a2-----------p2
-     The matching M1 is {'a1':'p2', 'p2':'a1'}
-     so O1, EV1 and U1 are {a1,p2},{a2,p1},{} respectively.
-     After removing the edges incident to O1 with the rank higher than 1
-     {(a1,p1),(a2,p2)} there are no more edges
-     to add to G1, so an augmenting path doesn't exist,
-     and the algorithm ends returning M1.
-
+    explanation:                            2
+                    G =             a1-----------p1
+                                     \
+                                      \
+                                       \
+                                        \
+                                         \\ 1
+                                          \
+                                           \
+                                            \
+                                       2     \
+                                a2-----------p2
+     The matching M1 is {'a1':'p2', 'p2':'a1'} so O1, EV1 and U1 are  {a1,p2},{a2,p1},{} respectively.
+     After removing the edges incident to O1 with the rank higher than 1 {(a1,p1),(a2,p2)} there are no more edges
+     to add to G1, so an augmenting path doesn't exist and the algorithm ends returning M1.
     another example:
         >>> G = nx.Graph()
         >>> G.add_nodes_from(["a1", "a2"])
         >>> G.add_nodes_from(["p1", "p2"])
-        >>> G.add_weighted_edges_from([("a1", "p2", 1), ("a1", "p1", 1),
-        ... ("a2", "p2", 2)])
+        >>> G.add_weighted_edges_from([("a1", "p2", 1), ("a1", "p1", 1), ("a2", "p2", 2)])
         >>> M = rank_maximal_matching(G, rank="weight", top_nodes=["a1", "a2"])
         >>> print(M=={"a1": "p1", "a2": "p2", "p1": "a1", "p2": "a2"})
         True
         >>> M['a1']
         'p1'
-
     Raises
     ------
     AmbiguousSolution
@@ -79,27 +66,20 @@ def rank_maximal_matching(G, rank="rank", top_nodes=None):
       with all nodes in one bipartite set is provided. When determining
       the nodes in each bipartite set more than one valid solution is
       possible if the input graph is disconnected.
-
     Notes
     -----
-    This function uses the algorithm published in the article of
-    Irving et al. (2006), "Rank maximal matching".
+    This function uses the algorithm published in the article of Irving et al. (2006), "Rank maximal matching".
     See :mod:`bipartite documentation <networkx.algorithms.bipartite>`
     for further details on how bipartite graphs are handled in NetworkX.
-
     See Also
     --------
     maximum_matching
     hopcroft_karp_matching
-
     References
     ----------
-    Irving, Robert W. and Kavitha, Telikepalli and Mehlhorn, Kurt and Michail,
-     Dimitrios and Paluch, Katarzyna E.,
-    "Rank-Maximal Matchings",ACM Trans. Algorithms,2006,
-    Association for Computing Machinery**
+    Irving, Robert W. and Kavitha, Telikepalli and Mehlhorn, Kurt and Michail, Dimitrios and Paluch, Katarzyna E.,
+    "Rank-Maximal Matchings",ACM Trans. Algorithms,2006,Association for Computing Machinery**
        https://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.92.6742&rep=rep1&type=pdf
-
     """
 
     if G.number_of_nodes() == 0 or G.number_of_edges() == 0:
