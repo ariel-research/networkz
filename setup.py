23b8cbbd0764065bf61feabc1b5a8d348e853722
oriyalperin <oriya2011@gmail.com>
2023-06-13 14:58:11 +0300

    add packages to setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ from setuptools import setup, find_packages
 with open('requirements/default.txt') as r:
     install_requires = r.readlines()
 
+packages = find_packages()
+
 setup(
     name='networky',
-    version='1.0.0',
+    version='1.0.1',
     author='Ariel University',
     author_email='ariel_research23@gmail.com',
     description='Extended Graph Library on Top of NetworkX',
-    packages=['networky'],
+    packages=packages,
     install_requires= install_requires,
 )
