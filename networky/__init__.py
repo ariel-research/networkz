46da9605e73cc6a43cf5e391572115de02191131
Oriya Alperin <73301483+oriyalperin@users.noreply.github.com>
2023-06-13 20:32:19 +0300

    remove unnecessary imports

--- networky/__init__.py
+++ networky/__init__.py
@@ -1,14 +1,5 @@
 from networkx import *
-import os
 
-# Get the current directory of the __init__.py file
-current_directory = os.path.dirname(__file__)
-
-# Append the 'algorithms' directory to the __path__ attribute
-__path__.append(os.path.join(current_directory, 'algorithms'))
-
-
-from networkx import lazy_imports
 from networky import algorithms
 from networky.algorithms import *
 
