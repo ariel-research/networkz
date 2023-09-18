725341fb02194202cfe6290db208e9342c465b2e
Erel Segal-Halevi <erelsgl@gmail.com>
2023-09-18 17:48:32 +0300

    rename to networkz; add version; change setup; add gitignore.

--- setup.py
+++ setup.py
@@ -1,16 +1,46 @@
-from setuptools import setup, find_packages
+import pathlib
+import setuptools
 
-with open('requirements/default.txt') as r:
-    install_requires = r.readlines()
+NAME = "networky"
+URL = "https://github.com/ariel-research/" + NAME
+HERE = pathlib.Path(__file__).parent
+print(f"\nHERE = {HERE.absolute()}\n")
 
-packages = find_packages()
+README = (HERE / "README.md").read_text()
+REQUIRES = (HERE / "requirements/default.txt").read_text().strip().split("\n")
+REQUIRES = [lin.strip() for lin in REQUIRES]
 
-setup(
+print(f'\nVERSION = {(HERE / NAME / "VERSION").absolute()}\n')
+VERSION = (HERE / NAME / "VERSION").read_text().strip()
+# See https://packaging.python.org/en/latest/guides/single-sourcing-package-version/
+
+setuptools.setup(
     name='networky',
-    version='1.0.2',
+    packages=setuptools.find_packages(),
+    version=VERSION,
+    install_requires=REQUIRES,
+
     author='Ariel University',
     author_email='networky@csariel.xyz',
     description='Extended Graph-Algorithms Library on Top of NetworkX',
-    packages=packages,
-    install_requires= install_requires,
+
+    url=URL,
+    project_urls={"Bug Reports": URL + "/issues", "Source Code": URL},
+
+    long_description=README,
+    long_description_content_type="text/markdown",
 )
+
+
+# Build:
+#   Delete old folders: build, dist, *.egg_info, .venv_test.
+#   Then run:
+#        python -m build
+#   Or (old version):
+#        python setup.py sdist bdist_wheel
+
+# Publish to test PyPI:
+#   twine upload --repository testpypi dist/*
+
+# Publish to real PyPI:
+#   twine upload --repository pypi dist/*
