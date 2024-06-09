bf05df4d0889742b379574ba0fb305c6c086b408
20shaked20 <shakedlevi32@gmail.com>
2024-06-09 12:11:05 +0300

    Removed uneeded code from lib

--- networkz/algorithms/approximation/firefighter_problem/Firefighter_Problem.py
+++ networkz/algorithms/approximation/firefighter_problem/Firefighter_Problem.py
@@ -393,15 +393,3 @@ def heuristic_minbudget(Graph:nx.DiGraph, source:int, targets:list, spreading:bo
     logger.info(f"Returning minimum budget: {middle}")
     return middle
     
-
-# if __name__ == "__main__":
-#     import doctest
-#     doctest.testmod(verbose=True)
-
-#     # G3 = nx.DiGraph() 
-#     # G3.add_nodes_from([0,1,2,3,4,5,6,7,8], status="vulnerable")
-#     # G3.add_edges_from([(0,2),(0,4),(0,5),(2,1),(2,3),(4,1),(4,6),(5,3),(5,6),(5,7),(6,7),(6,8),(7,8)])
-#     # logger.info("=" * 150)
-#     # logger.info(spreading_minbudget(G3,0,[2,6,1,8]))
-#     # logger.info("=" * 150)
-#     # logger.info(heuristic_minbudget(G3,0,[2,6,1,8], True))
