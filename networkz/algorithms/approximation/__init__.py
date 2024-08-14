458a7f1c884138cddf86be8cafc008e5cf8e6e23
20shaked20 <shakedlevi32@gmail.com>
2024-08-14 19:37:00 +0300

    init plz

--- networkz/algorithms/approximation/__init__.py
+++ networkz/algorithms/approximation/__init__.py
@@ -1,2 +1,2 @@
 from networkz.algorithms.approximation.coalition_formation import *
-from networkz.algorithms.approximation.firefighter_problem import *
+from networkz.algorithms.approximation.firefighter_problem.Firefighter_Problem import *
--- networkz/algorithms/approximation/firefighter_problem/__init__.py
+++ networkz/algorithms/approximation/firefighter_problem/__init__.py
@@ -1,1 +1,1 @@
-from networkz.algorithms.approximation.firefighter_problem import *
+from networkz.algorithms.approximation.firefighter_problem.Firefighter_Problem import *
